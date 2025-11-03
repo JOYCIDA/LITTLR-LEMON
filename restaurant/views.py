e78af297321cc0dc61cd6436fb8384dfcc04b88b
MOUAHA <mouahajoyce25@gmail.com>
2025-11-03 10:29:40 +0100

    Initial commit - Little Lemon

---
+++ restaurant/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+from datetime import datetime
+import json
+
+from .models import Booking
+from .forms import BookingForm
+from .models import Menu
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', {"menu": main_data})
+
+
+def display_menu_item(request, pk=None): 
+    if pk: 
+        menu_item = Menu.objects.get(pk=pk) 
+    else: 
+        menu_item = "" 
+    return render(request, 'menu_item.html', {"menu_item": menu_item})
+
+
+def book(request):
+    form = BookingForm()
+    context = {'form': form}
+    return render(request, 'book.html', context)
+
+
+@csrf_exempt
+def bookings(request):
+    if request.method == "POST":
+        data = json.load(request)
+        exist = Booking.objects.filter(
+            reservation_date=data['reservation_date']
+        ).filter(reservation_slot=data['reservation_slot']).exists()
+
+        if not exist:
+            booking = Booking(
+                first_name=data['first_name'],
+                reservation_date=data['reservation_date'],
+                reservation_slot=data['reservation_slot']
+            )
+            booking.save()
+        else:
+            return HttpResponse("{'error':1}", content_type='application/json')
+
+    date = request.GET.get('date', datetime.today().date())
+    bookings = Booking.objects.all().filter(reservation_date=date)
+    booking_json = serializers.serialize('json', bookings)
+    return HttpResponse(booking_json, content_type='application/json')
